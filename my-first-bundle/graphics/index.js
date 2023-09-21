0dd18ea239ad6e0be4e580317a5b55e45461045e
jesseraj <89629604+jesseraj@users.noreply.github.com>
2023-09-21 20:12:00 +0300

    Lisätty bundles kansion sisältö

---
+++ my-first-bundle/graphics/index.js
@@ -0,0 +1,7 @@
+// You can access the NodeCG api anytime from the `window.nodecg` object
+// Or just `nodecg` for short. Like this!:
+nodecg.log.info("Here's an example of using NodeCG's logging API!");
+
+nodecg.Replicant("speakerReplicant", { defaultValue: "Maija Mehiläinen" });
+
+nodecg.Replicant("roleReplicant", { defaultValue: "Mehiläinen" });
